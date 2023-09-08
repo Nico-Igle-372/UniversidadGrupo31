bdcc97b3b11beb130d52b4946ca338215eed69dc
Garaf <Mr.Churches@gmail.com>
2023-09-08 20:42:11 -0300

    Update Conexion.java

--- src/universidadgrupo31/accesoADatos/Conexion.java
+++ src/universidadgrupo31/accesoADatos/Conexion.java
@@ -3,25 +3,28 @@ package universidadgrupo31.accesoADatos;
 import java.sql.Connection;
 import java.sql.DriverManager;  // 
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Conexion{
-    private static final String URL ="jdbc:mariabd://localhost/";
-    private static final String BD = "universidadulp";
-    private static final String USUARIO = "root";    
-    private static final String PASSWORD = "";       
+    private static String url;
+    private static String BD;
+    private static String usuario;    
+    private static String password;       
     
     private static Connection connexion = null;  
-    
-    public Conexion(){ }
+
+    public Conexion(String url, String bd, String usu, String pass) {
+        this.url = url;
+        BD = bd;
+        usuario = usu;
+        password = pass;
+    }
       
     public static Connection getConexion(){
         if (connexion == null) { 
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
-                connexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+                connexion = DriverManager.getConnection(url+BD, usuario, password);
             }catch(ClassNotFoundException ex){ 
                 JOptionPane.showMessageDialog(null, "Error al cargar el Driver  \n" + ex);
             }catch (SQLException ex) {
